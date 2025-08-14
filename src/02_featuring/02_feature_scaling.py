73534248b69161e6a1ebeeafa4ac2c6697a1d823
Abhijeet Thombare <abhithombare45@gmail.com>
2025-08-15 01:54:02 +0530

    working on data Structure

---
+++ src/02_featuring/02_feature_scaling.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+
+df = pd.read_pickle("../data/interim/01_make_dataset.pkl")
+
+
+# matrix of feature / independent variable
+x = df.iloc[:, 1:-1].values
+x.shape
+
+# Target Variable / dependent variable
+y = df.iloc[:, -1].values
+y.shape
+
+# Import sklearn(scikit-learn) for spliting data
+# into train and Test dataset
+sktt = train_test_split()
+x_train, x_test, y_train, y_test = sktt(x, y, test_size=0.2, random_state=0)
+
+x_train.shape
+x_train
+x_test.shape
+x_test
+y_train.shape
+y_train
+y_test.shape
+y_test
+
+
+## Feature Scaling
+
+sc = StandardScaler()
+x_train = sc.fit_transform(x_train)
+x_test = sc.transform(x_test)
+
+x_train  # x_train.shape
+x_test  # x_test.shape
